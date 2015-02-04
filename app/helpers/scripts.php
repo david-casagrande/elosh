a2806a6dd353af47d509ab94b28fddd9648f3dac
David Casagrande <dncasagrande@gmail.com>
2015-02-03 22:42:24 -0500

    added production build

--- app/helpers/scripts.php
+++ app/helpers/scripts.php
@@ -1,12 +1,16 @@
 <?php
 
-$client_dir = get_template_directory_uri().'/elosh-client/bower_components/elosh-client/dist/assets/';
-$bower_dir = get_template_directory_uri().'/elosh-client/bower_components/';
+$client_dir = get_template_directory_uri().'/bower_components/elosh-client/dist/assets/';
+$foundation_dir = get_template_directory_uri().'/bower_components/foundation/css/';
+
+if(defined('APP_ENVIRONMENT') && constant('APP_ENVIRONMENT') == 'production') {
+  $client_dir = get_template_directory_uri().'/bower_components/elosh-client/dist_production/assets/';
+}
 
 $scripts = array(
   array('script' => 'vendor',       'src' => $client_dir.'vendor.js', 'load_in_footer' => true ),
   array('script' => 'elosh-client', 'src' => $client_dir.'elosh-client.js', 'load_in_footer' => true ),
-  array('style'  => 'foundation',   'src' => $bower_dir.'/foundation/css/foundation.css' ),
+  array('style'  => 'foundation',   'src' => $foundation_dir.'foundation.css' ),
   array('style'  => 'app',          'src' => $client_dir.'elosh-client.css' )
 );
 
